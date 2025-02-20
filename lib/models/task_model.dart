4445d8f7d4a878e8ba6aa719724b753dee9daa0a
KishuKing <142623708+KishuKing@users.noreply.github.com>
2025-02-20 20:57:13 +0530

    Update task_model.dart

--- lib/models/task_model.dart
+++ lib/models/task_model.dart
@@ -1,12 +1,21 @@
 class Task {
   String title;
   bool isCompleted;
+  String priority; // New field for priority
 
-  Task({required this.title, this.isCompleted = false});
+  Task({required this.title, this.isCompleted = false, this.priority = "low"});
 
-  Map<String, dynamic> toJson() => {'title': title, 'isCompleted': isCompleted};
+  Map<String, dynamic> toJson() => {
+    'title': title,
+    'isCompleted': isCompleted,
+    'priority': priority,
+  };
 
   factory Task.fromJson(Map<String, dynamic> json) {
-    return Task(title: json['title'], isCompleted: json['isCompleted']);
+    return Task(
+      title: json['title'],
+      isCompleted: json['isCompleted'],
+      priority: json['priority'] ?? "low", // Default priority if not set
+    );
   }
 }
