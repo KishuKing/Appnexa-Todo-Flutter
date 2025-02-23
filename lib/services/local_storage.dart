ac91a29e2022dd4f3bf726315873c8c57b61c842
KishuKing <142623708+KishuKing@users.noreply.github.com>
2025-02-23 13:51:20 +0530

    Update local_storage.dart

--- lib/services/local_storage.dart
+++ lib/services/local_storage.dart
@@ -2,9 +2,13 @@ import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../models/task_model.dart';
 
+/// Class responsible for storing and retrieving tasks using local storage.
 class LocalStorage {
-  static const String _key = "tasks";
+  static const String _key =
+      "tasks"; // Key used to store tasks in SharedPreferences.
 
+  /// Saves a list of tasks to local storage.
+  /// Each task is converted to a JSON string before being stored.
   Future<void> saveTasks(List<Task> tasks) async {
     final prefs = await SharedPreferences.getInstance();
     List<String> jsonList =
@@ -12,6 +16,8 @@ class LocalStorage {
     await prefs.setStringList(_key, jsonList);
   }
 
+  /// Loads a list of tasks from local storage.
+  /// The stored JSON strings are converted back to Task objects.
   Future<List<Task>> loadTasks() async {
     final prefs = await SharedPreferences.getInstance();
     List<String>? jsonList = prefs.getStringList(_key);
